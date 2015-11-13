29d3649247c759523038e8c64e5838744513d519
EC2 Default User <ec2-user@ip-172-31-27-230.us-west-2.compute.internal>
2015-11-13 23:24:02 +0000

    A

--- bullyalgorithm.py
+++ bullyalgorithm.py
@@ -97,27 +97,26 @@ def TCPServer_wait_OK():
     TCP_PORT = Configuration.TCPPORT_OK 
     BUFFER_SIZE = 1024
     print threading.currentThread().getName(), 'TCP OK OK OK Server Starting. I am Node#', ID, "ip=", MYIP
-    try:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(( socket.gethostname(), TCP_PORT))
-        print "TCP OK OK OK Server at", socket.gethostname(), ":", TCP_PORT
-        server.listen(5) #At most 5 concurrent connection
-        timeout_in_seconds = 10
-        ready = select.select([server], [], [], timeout_in_seconds)
-        if ready[0]:
-            data = server.recv(4096)
-            print "RECEIVE OK"
-            return True
-        print "DID NOT RECEIVE OK"
-        return False
-    except:
-        print "OK Server Conflict"
-        return False
+    #try:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(( socket.gethostname(), TCP_PORT))
+    print "TCP OK OK OK Server at", socket.gethostname(), ":", TCP_PORT
+    server.listen(5) #At most 5 concurrent connection
+    timeout_in_seconds = 10
+    ready = select.select([server], [], [], timeout_in_seconds)
+    if ready[0]:
+        data = server.recv(4096)
+        print "RECEIVE OK"
+        return True
+    print "DID NOT RECEIVE OK"
+    return False
+    #except:
+    #print "OK Server Conflict"
+    #return False
 
 #============================ main =========================#
 
 leader = -1 # unknown leader 
-
 
 tTCPServer = threading.Thread(target=TCPServer)
 tTCPServer.daemon = True
